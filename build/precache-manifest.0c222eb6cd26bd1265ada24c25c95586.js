8bb417da4b9385b8f468af675cbb34702bf2621e
moelatt <moelatt.cs@gmail.com>
2019-07-23 21:29:01 -0700

    update

---
+++ build/precache-manifest.0c222eb6cd26bd1265ada24c25c95586.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a8a445acf8f92c710d7cef8a3f71ac",
+    "url": "/List_To_Do_with_React/index.html"
+  },
+  {
+    "revision": "480d6b940880cc09aff8",
+    "url": "/List_To_Do_with_React/static/css/main.52b350ed.chunk.css"
+  },
+  {
+    "revision": "5b9c0168d608aa95de00",
+    "url": "/List_To_Do_with_React/static/js/2.0dcb8f84.chunk.js"
+  },
+  {
+    "revision": "480d6b940880cc09aff8",
+    "url": "/List_To_Do_with_React/static/js/main.c5b5e487.chunk.js"
+  },
+  {
+    "revision": "5722895ce16c2994dde4",
+    "url": "/List_To_Do_with_React/static/js/runtime~main.9b1d66e7.js"
+  }
+]);
