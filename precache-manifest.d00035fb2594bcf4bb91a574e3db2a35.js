328bfd96c579e5980973c7270f0d18945e76172c
moelatt <moelatt.cs@gmail.com>
2019-07-23 18:21:49 -0700

    Updates

---
+++ precache-manifest.d00035fb2594bcf4bb91a574e3db2a35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d1f67ee2d2f3e621cc3dd4d18aa797",
+    "url": "/List_To_Do_React/index.html"
+  },
+  {
+    "revision": "267206f6b3026277e9f3",
+    "url": "/List_To_Do_React/static/css/main.52b350ed.chunk.css"
+  },
+  {
+    "revision": "5b9c0168d608aa95de00",
+    "url": "/List_To_Do_React/static/js/2.0dcb8f84.chunk.js"
+  },
+  {
+    "revision": "267206f6b3026277e9f3",
+    "url": "/List_To_Do_React/static/js/main.bb439349.chunk.js"
+  },
+  {
+    "revision": "356b404a1df9666d3560",
+    "url": "/List_To_Do_React/static/js/runtime~main.3dff8e9e.js"
+  }
+]);
