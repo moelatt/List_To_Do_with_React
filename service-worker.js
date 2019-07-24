eb310b78a2a9de982c3df60d13e047dd90beabfd
moelatt <moelatt.cs@gmail.com>
2019-07-23 21:08:05 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/List_To_Do_React/precache-manifest.d00035fb2594bcf4bb91a574e3db2a35.js"
+  "/List_To_Do_with_React/precache-manifest.0c222eb6cd26bd1265ada24c25c95586.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/List_To_Do_React/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/List_To_Do_with_React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
